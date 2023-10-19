80dedc84c41e96d4fc97400fdb56e87ca70cf19c
reduno96 <reduno96@gmail.com>
2023-10-19 17:33:36 +0100

    modification memset function

--- libft/memset.c
+++ libft/memset.c
@@ -6,22 +6,29 @@
 /*   By: motivred <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 17:52:16 by motivred          #+#    #+#             */
-/*   Updated: 2023/10/18 00:41:11 by motivred         ###   ########.fr       */
+/*   Updated: 2023/10/19 17:19:46 by motivred         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
-void *memset(void *s, int c, unsigned int n)
+void *ft_memset(void *s, int c, size_t n)
 {
-	
-}
+	char *str;
 
+	str = s;
+	while (str[n])
+		str[n++] = (unsigned char)c;
+	return str;
+
+}
+/*
 int main ()
 {
 	char str [20] = "redouane el morabet";
 	char des = '.';
-	memset(str + 13,des,sizeof(char));
+	ft_memset(str + 13, des, 3*sizeof(char)); 
 	printf("%s", str);
-}
+	}
+	*/
